a3fc43007251a2085148627d6986a4ea122502d5
Marcus Wong <marcus.wong@qualicom.com.hk>
2022-11-08 01:03:34 +0800

    rename

---
+++ lambda_resume/count_visit/app.py
@@ -0,0 +1,45 @@
+import json
+import boto3
+# import requests
+from boto3.dynamodb.conditions import Key, Attr
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    
+    table = dynamodb.Table('resume_app_state')
+
+    
+    response = table.get_item(Key={'id': 'counter'})
+    item = response['Item']
+
+    # TODO: overflow ?? when you page has so many visitor..
+    count=int(item["value"])
+    count=count +1
+
+    table.update_item(
+        Key={'id': 'counter'},
+        UpdateExpression="set #v = :g",
+        ExpressionAttributeValues={
+                ':g': str(count)
+            },
+        ExpressionAttributeNames={
+        "#v": "value"
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+
+
+    return {
+        "statusCode": 200,
+         "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "*",
+        },
+        "body": json.dumps({
+            "count": str(count),
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
